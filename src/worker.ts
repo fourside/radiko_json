daeedb876929f3df4077e2546e76bafc743bd7ef
fourside <fourside@gmail.com>
2023-08-15 10:22:44 +0900

    remove comments

--- src/worker.ts
+++ src/worker.ts
@@ -1,37 +1,8 @@
 import { XMLParser } from 'fast-xml-parser';
 import { array, number, object, safeParse, string } from 'valibot';
-/**
- * Welcome to Cloudflare Workers!
- *
- * This is a template for a Scheduled Worker: a Worker that can run on a
- * configurable interval:
- * https://developers.cloudflare.com/workers/platform/triggers/cron-triggers/
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
 
 export interface Env {
-	// Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-	// MY_KV_NAMESPACE: KVNamespace;
-	//
-	// Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-	// MY_DURABLE_OBJECT: DurableObjectNamespace;
-	//
-	// Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
 	MY_BUCKET: R2Bucket;
-	//
-	// Example binding to a Service. Learn more at https://developers.cloudflare.com/workers/runtime-apis/service-bindings/
-	// MY_SERVICE: Fetcher;
-	//
-	// Example binding to a Queue. Learn more at https://developers.cloudflare.com/queues/javascript-apis/
-	// MY_QUEUE: Queue;
-	//
-	// Example binding to a D1 Database. Learn more at https://developers.cloudflare.com/workers/platform/bindings/#d1-database-bindings
-	// DB: D1Database
 }
 
 export default {
@@ -56,10 +27,8 @@ export default {
 		if (request.method.toLowerCase() !== 'get') {
 			return new Response(null, { status: 405 });
 		}
-		// For example, the request URL my-worker.account.workers.dev/image.png
 		const url = new URL(request.url);
 		const key = url.pathname.slice(1);
-		// Retrieve the key "image.png"
 		const object = await env.MY_BUCKET.get(key);
 
 		if (object === null) {
